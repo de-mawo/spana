f3075e03bbf9977a0e5f47610f1646e188a20c4d
de-mawo <groovebassist1@gmail.com>
2023-03-07 16:31:47 +0200

    Added a pathname highlight link on sidebar

--- app/dashboard/SideBar.tsx
+++ app/dashboard/SideBar.tsx
@@ -1,5 +1,6 @@
 import { signOut } from "next-auth/react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import {
   HiOutlineArrowRightOnRectangle,
@@ -13,6 +14,9 @@ type Props = {
 };
 
 const SideBar = ({ show, showSideBar }: Props) => {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <aside className={`dash_sidebar ${show && "md:w-[14rem]"}`}>
       <HiOutlineChevronDoubleRight
@@ -40,7 +44,11 @@ const SideBar = ({ show, showSideBar }: Props) => {
             {AdminRoutes.map((route, index) => (
               <Link
                 href={route.url}
-                className="flex items-center py-4 pl-3 text-deep-sapphire-900 transition-all hover:bg-deep-sapphire-300 hover:text-white dark:text-gray-300 dark:hover:bg-gray-600"
+                className={
+                  pathname?.endsWith(route.url)
+                    ? " flex items-center py-4 pl-3  transition-all bg-deep-sapphire-300 underline text-white dark:text-gray-300 dark:bg-gray-600"
+                    : "flex items-center py-4 pl-3 text-deep-sapphire-900 transition-all hover:bg-deep-sapphire-300 hover:text-white dark:text-gray-300 dark:hover:bg-gray-600"
+                }
                 key={index}
               >
                 {route.icon}
@@ -53,11 +61,11 @@ const SideBar = ({ show, showSideBar }: Props) => {
         </div>
 
         <button
-         onClick={() => signOut({ callbackUrl: '/' })}
+          onClick={() => signOut({ callbackUrl: "/" })}
           className="flex items-center py-2 pl-3 text-deep-sapphire-900 transition-all hover:bg-deep-sapphire-300 hover:text-white dark:text-gray-300 dark:hover:bg-gray-600"
         >
           {" "}
-          <span className=""  >
+          <span className="">
             <HiOutlineArrowRightOnRectangle className="h-6 w-6  mr-4 shrink-0" />{" "}
           </span>
           <span className={`pl-4 ${!show && "hidden"}`}>Logout</span>{" "}
