d55f4018c784fe67fd0041c25ca8d67c25a26a3e
groovebass <groovebassist1@gmail.com>
2023-05-18 07:59:12 +0200

    Refactoring of the request list file

--- app/dashboard/admin/approvals/RequestList.tsx
+++ app/dashboard/admin/approvals/RequestList.tsx
@@ -1,29 +1,26 @@
 "use client";
 
 import { withUrqlClient } from "next-urql";
-import { useGetUnModeratedLeavesQuery } from "../../../../graphql/generated";
+import { useGetUnModeratedLeavesQuery, useGetUserBalancesQuery } from "../../../../graphql/generated";
 import { createUrqlClient } from "../../../../lib/createUrqlClient";
 import LoadingComponent from "../../../LoadingComponent";
 import RequestModal from "./RequestModal";
 
 const RequestList = () => {
-  const [{ data, fetching, error }] = useGetUnModeratedLeavesQuery();
-
-
-
+  const [{ data: unmoderatedData, fetching, error }] = useGetUnModeratedLeavesQuery();
+  
   if (fetching) return <LoadingComponent />;
 
-  if (data?.getUnModeratedLeaves.length === 0)
+  if (unmoderatedData?.getUnModeratedLeaves.length === 0)
     return (
       <div className="text-center my-5">
-        {" "}
-        <p className="text-lg font-bold py-4">No Requests Found... </p>{" "}
+        <p className="text-lg font-bold py-4">No Requests Found...</p>
       </div>
     );
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 py-10 gap-5">
-      {data?.getUnModeratedLeaves.map((requested) => (
+      {unmoderatedData?.getUnModeratedLeaves.map(requested => (
         <RequestModal key={requested.id} requested={requested} pageProps={undefined} />
       ))}
     </div>
